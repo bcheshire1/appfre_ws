4acdc965870b4a981d9bd4fffffa91afe98d7ac9
Ben Cheshire <b.cheshire1@lancaster.ac.uk>
2024-02-05 16:49:16 +0000

    built workspace with radiacode package

---
+++ build/radiacode/CMakeFiles/radiacode_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radiacode_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/radiacode_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
