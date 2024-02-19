53b29b8517e5f1bef595dbcaa42afea7ab72aafa
Nathan Leadbetter <149001851+n-leadbetter@users.noreply.github.com>
2024-02-19 17:02:54 +0000

    Create Socket_Server_Test.py

---
+++ src/Pi2Pi_Comms_Protocol/Socket_Server_Test.py
@@ -0,0 +1,28 @@
+import socket
+
+# Define server address and port
+HOST = '0.0.0.0'  # Listen on all available interfaces
+PORT = 12345      # Arbitrary port number
+
+# Create a socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    # Bind the socket to the address and port
+    server_socket.bind((HOST, PORT))
+
+    # Listen for incoming connections
+    server_socket.listen()
+
+    print("Server listening on {}:{}".format(HOST, PORT))
+
+    # Accept a connection
+    client_socket, client_address = server_socket.accept()
+
+    with client_socket:
+        print("Connected to client:", client_address)
+
+        # Receive data from the client
+        data = client_socket.recv(1024)
+        print("Received:", data.decode())
+
+        # Send a response back to the client
+        client_socket.sendall(b"Hello from server!")
